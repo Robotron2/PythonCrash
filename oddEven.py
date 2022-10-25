928583c559920eab8dce51583902f3745d666b75
Theophilus <theophilusadesola002@gmail.com>
2022-10-25 08:30:31 +0100

    odd or even number checker

---
+++ oddEven.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 08:27:13 2022
+
+@author: Theophilus
+"""
+
+userNum = int(input("Enter any number: "))
+
+if (userNum % 2 == 0):
+    print(f"{userNum} is even")
+else:
+    print(f"{userNum} is odd")
